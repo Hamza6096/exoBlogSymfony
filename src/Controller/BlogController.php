b826c37abc5f2172b30428c0198f944917e7dde0
Hamza <noualihamza@hotmail.com>
2022-05-18 02:29:13 +0200

    add routin

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ArticleRepository $repo ): Response
     {
+        $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
     /**
@@ -25,9 +31,12 @@ class BlogController extends AbstractController
         ]);
     }
     /** 
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show() {
-        return $this->render('blog/show.html.twig');
+    public function show(ArticleRepository $repo, $id) {
+        $articles = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'article' => $articles
+        ]);
     }
 }
